5c97ca011219526447daac2e4222c66b0a19ef9e
Rodri-Zelaya <rzelayasalinas7@gmail.com>
2026-05-12 00:01:57 -0600

    X

--- obj/Debug/net8.0-windows/Métodos Numéricos.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Métodos Numéricos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Métodos Numéricos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5e89ce94d868dd2680e67d4e1d99da60593eb00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07975783c2dcbcf58bb2f111bdc029c68bb50fad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Métodos Numéricos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Métodos Numéricos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
